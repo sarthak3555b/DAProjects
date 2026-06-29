496aee4fcc1a53b35ebd7542cb0a857ef8f37d76
sarthak3555b <sarthakrawat190072@gmail.com>
2026-06-30 01:26:32 +0530

    Delete wealth-mastery-saas directory

--- wealth-mastery-saas/src/app/(app)/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function AppLoading() {
-  return (
-    <div className="flex h-full items-center justify-center p-12">
-      <div className="h-8 w-8 animate-spin rounded-full border-[3px] border-border border-t-learning" />
-    </div>
-  );
-}
